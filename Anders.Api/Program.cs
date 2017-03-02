2c44fe0b01f28afe703d94030805dd8fbdc9fa73
Andrew Withers <andreww@thomasdev.co.uk>
2017-03-02 12:26:50 +0000

    Add API project with identity server protection

---
+++ Anders.Api/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Anders.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.Title = "Anders API";
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls("http://localhost:5001")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
